8061172ab2895f36ee68010baf9281f8ae6270d1
Sarfaraz <khansarfarazali0@gmail.com>
2023-01-18 15:26:54 +0530

    first

---
+++ webserver/wsgi.py
@@ -0,0 +1,4 @@
+from application import application
+
+if name == "__main__":
+    application.run()
---
+++ worker/default_config.py
@@ -0,0 +1,5 @@
+# Default app config
+AWS_REGION = 'ap-south-1'
+FLASK_DEBUG = 'false'
+SOURCE_EMAIL_ADDRESS = 'khansarfarazali0@gmail.com'
+
